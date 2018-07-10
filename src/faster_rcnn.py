b3695f1c606e61ee3f9b33e96b2dac0e3925ae30
taashi-s <x7286x_ayk@yahoo.co.jp>
2018-07-10 14:00:02 +0900

    Add head loss Layer

--- src/faster_rcnn.py
+++ src/faster_rcnn.py
@@ -12,6 +12,9 @@ from subnetwork.resnet.resnet import ResNet
 from subnetwork.rpn.region_proporsal_net import RegionProporsalNet
 from layers.rp_class_loss import RPClassLoss
 from layers.rp_region_loss import RPRegionLoss
+from layers.detection_target_region import DetectionTargetRegion
+from layers.class_loss import ClassLoss
+from layers.region_loss import RegionLoss
 
 class TrainTarget(Enum):
     """
@@ -37,34 +40,44 @@ class FasterRCNN():
         train_rpn = TrainTarget.RPN in train_taegets
         train_head = TrainTarget.HEAD in train_taegets
 
-        inputs = Input(self.__input_shape)
+        inputs = []
+        outputs = []
 
-        resnet = ResNet(inputs.get_shape(), input_layers=inputs
+        inputs_images = Input(shape=self.__input_shape)
+        inputs += [inputs_images]
+
+        resnet = ResNet(inputs_images.get_shape(), input_layers=inputs_images
                         , trainable=train_backbone).get_residual_network()
 
         rpn = RegionProporsalNet(resnet.get_shape(), input_layers=resnet
                                  , trainable=train_rpn).get_network()
 
-        lossees_list = []
         if train_rpn:
-            # calculate rpn loss, and append to lossees_list
-            cla_losses = RPClassLoss()(rpn)
-            reg_losses = RPRegionLoss()(rpn)
-            lossees_list += [cla_losses, reg_losses]
+            inputs_rp_cls = Input(shape=[None, 1], dtype='int32')
+            inputs_rp_reg = Input(shape=[None, 4], dtype='float32')
+            inputs += [inputs_rp_cls, inputs_rp_reg]
+
+            rp_cls_losses = RPClassLoss()(rpn)
+            rp_reg_losses = RPRegionLoss()(rpn)
+            outputs += [rp_cls_losses, rp_reg_losses]
 
         if train_head:
-            # add head layer
-            #__head_net()
+            dtr = DetectionTargetRegion()(rpn)
+            head = self.__head_net(dtr)
 
-            # calculate rpn loss, and append to lossees_list
-            lossees_list += []
+            inputs_cls = Input(shape=[None, 1], dtype='int32')
+            inputs_reg = Input(shape=[None, 4], dtype='float32')
+            inputs += [inputs_cls, inputs_reg]
 
-        outputs = lossees_list
+            cls_losses = ClassLoss()(head)
+            reg_losses = RegionLoss()(head)
+            outputs += [cls_losses, reg_losses]
+
         self.__network = outputs
         self.__model = Model(inputs=[inputs], outputs=[outputs])
 
-        for lossees in lossees_list:
-            self.__model.add_loss(tf.reduce_mean(lossees, keep_dims=True))
+        for output in outputs:
+            self.__model.add_loss(tf.reduce_mean(output, keep_dims=True))
 
 
     def __head_net(self, inputs):
